3639afbc8ef72a03d8f6b33853116aacbca7a098
Rodfc773 <56032656+Rodfc773@users.noreply.github.com>
2020-11-30 17:29:39 -0300

    Update 10_teromachinesparaNcongruencias.c

--- 10_teromachinesparaNcongruencias.c
+++ 10_teromachinesparaNcongruencias.c
@@ -95,97 +95,113 @@ int main()
 
     preencher(0, 0, congruencia, matriz);
 
+    int verificar = 0;
 
-    int M = 1;
-
-    for (int i = 0; i < congruencia; i++)
-    {
-        M = matriz[i][1] * M;
-    }
-    for (int i = 0; i < congruencia; i++)
-    {
-        matriz[i][2] = M/matriz[i][1];
-    }
-    
-    for (int i = 0; i < congruencia; i++)
-    {
-        int tamanho = linhas(matriz[i][2], matriz[i][1], 0);
-
-        int array[tamanho][2];
-        long int a, m;
-
-        memset(array, 0, sizeof(array));
-
-        array[tamanho-1][1] = 1;
-        a = matriz[i][2];
-        m = matriz[i][1];
-
-        for (int j = 0; j < tamanho; j++)
-        {
-            int q = a/m;
-            int r = a%m;
-
-            a = m;
-            m = r;
-
-            array[j][0] = q;
-
-            if (r == 0)
-            {
-                break;
-            }
-            
-        }
-        
-        calculo(tamanho - 2, tamanho, array, 0);
-        
-        long int s = array[1][1];
-
-        if((tamanho - 1) % 2 == 0)
-        {
-            s = s * -1;
-        }
-        long int x = s;
-
-        if(x > matriz[i][1])
-        {
-            x = x % matriz[i][1];
-        }
-        matriz[i][3] = x;
-
-    }
-    
     long int Solucao;
 
     for (int i = 0; i < congruencia; i++)
     {
-        Solucao += matriz[i][2] * matriz[i][0] * matriz[i][3];
-    }
-    
-    printf("\n");
-
-    for (int i = 0; i < congruencia; i++)
-    {
-        for (int j = 0; j < 4; j++)
+        if(MDC(matriz[i][1], matriz[i + 1][1]) == 1 || i + 1 == congruencia)
         {
-            if(j + 1 == 4)
+            verificar = 1;
+        }
+        else
+        {
+            verificar = 0;
+        }    
+    }
+    if(verificar == 0)
+    {
+        printf("Os módulos não são coprimos\n");
+    } 
+    else
+    {
+        int M = 1;
+
+        for (int i = 0; i < congruencia; i++)
+        {
+            M = matriz[i][1] * M;
+        }
+        for (int i = 0; i < congruencia; i++)
+        {
+            matriz[i][2] = M/matriz[i][1];
+        }
+        
+        for (int i = 0; i < congruencia; i++)
+        {
+            int tamanho = linhas(matriz[i][2], matriz[i][1], 0);
+
+            int array[tamanho][2];
+            long int a, m;
+
+            memset(array, 0, sizeof(array));
+
+            array[tamanho-1][1] = 1;
+            a = matriz[i][2];
+            m = matriz[i][1];
+
+            for (int j = 0; j < tamanho; j++)
             {
-                printf("%d\n", matriz[i][j]);
+                int q = a/m;
+                int r = a%m;
+
+                a = m;
+                m = r;
+
+                array[j][0] = q;
+
+                if (r == 0)
+                {
+                    break;
+                }
+                
             }
-            else
+            
+            calculo(tamanho - 2, tamanho, array, 0);
+            
+            long int s = array[1][1];
+
+            if((tamanho - 1) % 2 == 0)
             {
-                printf("%d ", matriz[i][j]);
+                s = s * -1;
+            }
+            long int x = s;
+
+            if(x > matriz[i][1])
+            {
+                x = x % matriz[i][1];
+            }
+            matriz[i][3] = x;
+
+        } 
+
+        for (int i = 0; i < congruencia; i++)
+        {
+            Solucao += matriz[i][2] * matriz[i][0] * matriz[i][3];
+        }
+        for (int i = 0; i < congruencia; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                if(j + 1 == 4)
+                {
+                    printf("%d\n", matriz[i][j]);
+                }
+                else
+                {
+                    printf("%d ", matriz[i][j]);
+                }
+                
             }
             
         }
+        if (Solucao > M)
+        {
+            Solucao = Solucao % M;
+        }
         
-    }
-    if (Solucao > M)
-    {
-        Solucao = Solucao % M;
-    }
-    
-    printf("Solução das congruências é: %ld\n", Solucao);
+        printf("Solução das congruências é: %ld\n", Solucao);
+    } 
     return 0;
 
 }
